93ee32e75126dbd9a5acdc4e4be45384e999dc4e
Ricardo Luís <65141488+Ricardo-Luis@users.noreply.github.com>
2023-08-25 20:54:13 +0100

    Update sidebar data.jl

--- content/sidebar data.jl
+++ content/sidebar data.jl
@@ -1,15 +1,14 @@
 Dict(
     :main => [
         "welcome" => collections["welcome"].pages,
-        "Preliminaries" => collections["preliminaries"].pages,
-        "2: Social Science & Data Science" => collections["module2"].pages,
+        "Intro documentos computacionais" => collections["preliminaries"].pages,
+        "2: Máquina Elétrica de Corrente Contínua" => collections["module2"].pages,
         #"Module 3: Climate Science" => collections["module3"].pages,
 
     ],
     :about => Dict(
         :authors => [
-            (name = "Ricardo Luís", url = "https://ricardo-luis.github.io/"),
-            (name = "Second Instructor", url = "https://www.the-second.com")
+            (name = "Ricardo Luís", url = "https://ricardo-luis.github.io/")
         ],
         :title => "Máquinas Elétricas II",
         :subtitle => "Reactive notebooks",
